370f8dc331726f0789976ceb7331d77c0123a3c4
meralium <polybreaker@gmail.com>
2020-02-16 16:31:07 +0100

    - moved to src folder

---
+++ TinyRendererConsoleApp/src/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace TinyRendererConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using var image = new Image<Rgba32>(40, 40);
+            image[0,0] = Rgba32.Red;
+            image[10,10] = Rgba32.Red;
+            image[20,20] = Rgba32.Red;
+            image[30,30] = Rgba32.Red;
+
+            using var fs = File.Create($"image.png");
+            image.SaveAsPng(fs);
+        }
+    }
+}
