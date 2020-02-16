a8aa0b2edd00379a9a8bb32f244e0a79fd26064e
meralium <polybreaker@gmail.com>
2020-02-16 21:50:14 +0100

    - first lines

--- TinyRendererConsoleApp/src/Program.cs
+++ TinyRendererConsoleApp/src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Png;
@@ -11,15 +12,77 @@ namespace TinyRendererConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            using var image = new Image<Rgba32>(100, 100);
 
-            using var image = new Image<Rgba32>(40, 40);
-            image[0,0] = Rgba32.Red;
-            image[10,10] = Rgba32.Red;
-            image[20,20] = Rgba32.Red;
-            image[30,30] = Rgba32.Red;
+            // Renderer.SetLine5(image, 13, 20, 80, 40, Rgba32.Yellow);
+            // Renderer.SetLine5(image, 20, 13, 40, 80, Rgba32.White);
+            // Renderer.SetLine5(image, 80, 40, 13, 20, Rgba32.Red);
+            
+            Test(image);
 
-            using var fs = File.Create($"image.png");
-            image.SaveAsPng(fs);
+            // SAVE TO DISK
+            var encoder = new PngEncoder()
+            {
+                CompressionLevel = 1,
+            };
+
+            if (!Directory.Exists("output/"))
+            {
+                Directory.CreateDirectory("output/");
+            }
+
+            image.Save("output/image2.png", encoder);
+        }
+
+        private static void Test(Image<Rgba32> image)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                Renderer.SetLine3(image, 13, 20, 80, 40, Rgba32.Yellow);
+                Renderer.SetLine3(image, 20, 13, 40, 80, Rgba32.White);
+                Renderer.SetLine3(image, 80, 40, 13, 20, Rgba32.Red);
+            }
+
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+            sw.Restart();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                Renderer.SetLine4(image, 13, 20, 80, 40, Rgba32.Yellow);
+                Renderer.SetLine4(image, 20, 13, 40, 80, Rgba32.White);
+                Renderer.SetLine4(image, 80, 40, 13, 20, Rgba32.Red);
+            }
+
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+
+            sw.Restart();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                Renderer.SetLine5(image, 13, 20, 80, 40, Rgba32.Yellow);
+                Renderer.SetLine5(image, 20, 13, 40, 80, Rgba32.White);
+                Renderer.SetLine5(image, 80, 40, 13, 20, Rgba32.Red);
+            }
+
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+            
+            sw.Restart();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                Renderer.SetLine6(image, 13, 20, 80, 40, Rgba32.Yellow);
+                Renderer.SetLine6(image, 20, 13, 40, 80, Rgba32.White);
+                Renderer.SetLine6(image, 80, 40, 13, 20, Rgba32.Red);
+            }
+
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);       
         }
     }
 }
---
+++ TinyRendererConsoleApp/src/Renderer.cs
@@ -0,0 +1,232 @@
+using System;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace TinyRendererConsoleApp
+{
+    public class Renderer
+    {
+        public static void SetLine1(Image<Rgba32> image, int x0,int y0, int x1, int y1, Rgba32 color)
+        {
+            for (float t = 0; t < 1; t += 0.1f)
+            {
+                int x = (int)(x0 + (x1 - x0) * t);
+                int y = (int)(y0 + (y1 - y0) * t);
+                
+                image[x,y] = color;
+            }
+        }
+        
+        public static void SetLine2(Image<Rgba32> image, int x0, int y0, int x1, int y1, Rgba32 color)
+        {
+            for (var x = x0; x <= x1; x++)
+            {
+                float t = (x - x0) / (float) (x1 - x0);
+                int y = (int) (y0 * (1 - t) + y1 * t);
+                
+                image[x, y] = color;
+            }
+        }
+        
+        public static void SetLine3(Image<Rgba32> image, int x0, int y0, int x1, int y1, Rgba32 color)
+        {
+            var steep = Math.Abs(x0 - x1) < Math.Abs(y0 - y1);
+            if (steep)
+            {
+                var tmp = x0;
+                x0 = y0;
+                y0 = tmp;
+                
+                tmp = x1;
+                x1 = y1;
+                y1 = tmp;
+            }
+
+            if (x0 > x1)
+            {
+                var tmp = x0;
+                x0 = x1;
+                x1 = tmp;
+
+                tmp = y0;
+                y0 = y1;
+                y1 = tmp;
+            }
+
+            var dx = x1 - x0;
+            var dy = y1 - y0;
+            for (int x = x0; x <= x1; x++)
+            {
+                var t = (x - x0) / (float)(x1 - x0);
+                int y = (int)(y0 * (1 - t) + y1 * t);
+                if (steep)
+                {
+                    image[y, x] = color;
+                }
+                else
+                {
+                    image[x, y] = color;
+                }
+            }
+        }
+        
+        public static void SetLine4(Image<Rgba32> image, int x0, int y0, int x1, int y1, Rgba32 color)
+        {
+            var steep = Math.Abs(x0 - x1) < Math.Abs(y0 - y1);
+            if (steep)
+            {
+                var tmp = x0;
+                x0 = y0;
+                y0 = tmp;
+                
+                tmp = x1;
+                x1 = y1;
+                y1 = tmp;
+            }
+
+            if (x0 > x1)
+            {
+                var tmp = x0;
+                x0 = x1;
+                x1 = tmp;
+
+                tmp = y0;
+                y0 = y1;
+                y1 = tmp;
+            }
+
+            var dx = Math.Abs(x1 - x0);
+            var dy = Math.Abs(y1 - y0);
+            float derror = dy / (float)dx;
+            float error = 0;
+
+            int y = y0;
+            for (int x = x0; x <= x1; x++)
+            {
+                if (steep)
+                {
+                    image[y, x] = color;
+                }
+                else
+                {
+                    image[x, y] = color;
+                }
+
+                error += derror;
+
+                if (!(error > 0.5f)) continue;
+                y += y1 > y0 ? 1 : -1;
+                error -= 1f;
+            }
+
+        }
+        
+        public static void SetLine5(Image<Rgba32> image, int x0, int y0, int x1, int y1, Rgba32 color)
+        {
+            var steep = Math.Abs(x0 - x1) < Math.Abs(y0 - y1);
+            if (steep)
+            {
+                var tmp = x0;
+                x0 = y0;
+                y0 = tmp;
+                
+                tmp = x1;
+                x1 = y1;
+                y1 = tmp;
+            }
+
+            if (x0 > x1)
+            {
+                var tmp = x0;
+                x0 = x1;
+                x1 = tmp;
+
+                tmp = y0;
+                y0 = y1;
+                y1 = tmp;
+            }
+
+            var dx = Math.Abs(x1 - x0);
+            var dy = Math.Abs(y1 - y0);
+            int derror2 = dy * 2;
+            int error2 = 0;
+
+            int y = y0;
+            for (int x = x0; x <= x1; x++)
+            {
+                if (steep)
+                {
+                    image[y, x] = color;
+                }
+                else
+                {
+                    image[x, y] = color;
+                }
+
+                error2 += derror2;
+
+                if (!(error2 > dx)) continue;
+                y += y1 > y0 ? 1 : -1;
+                error2 -= dx * 2;
+            }
+
+        }
+        
+        public static void SetLine6(Image<Rgba32> image, int x0, int y0, int x1, int y1, Rgba32 color)
+        {
+            var steep = Math.Abs(x0 - x1) < Math.Abs(y0 - y1);
+            if (steep)
+            {
+                var tmp = x0;
+                x0 = y0;
+                y0 = tmp;
+                
+                tmp = x1;
+                x1 = y1;
+                y1 = tmp;
+            }
+
+            if (x0 > x1)
+            {
+                var tmp = x0;
+                x0 = x1;
+                x1 = tmp;
+
+                tmp = y0;
+                y0 = y1;
+                y1 = tmp;
+            }
+
+            var dx = Math.Abs(x1 - x0);
+            var dy = Math.Abs(y1 - y0);
+            int derror2 = dy * 2;
+            int error2 = 0;
+
+            int y = y0;
+
+            if (steep)
+            {
+                for (int x = x0; x <= x1; x++)
+                {
+                    image[y, x] = color;
+                    error2 += derror2;
+                    if (!(error2 > dx)) continue;
+                    y += y1 > y0 ? 1 : -1;
+                    error2 -= dx * 2;
+                }
+            }
+            else
+            {
+                for (int x = x0; x <= x1; x++)
+                {
+                    image[x, y] = color;
+                    error2 += derror2;
+                    if (!(error2 > dx)) continue;
+                    y += y1 > y0 ? 1 : -1;
+                    error2 -= dx * 2;
+                }
+            }
+        }
+        
+    }
+}
