14d1109b12c9cb7aeb343bd18388ace28750f5be
meralium <polybreaker@gmail.com>
2020-02-16 16:07:11 +0100

    - init commit

---
+++ TinyRendererConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TinyRendererConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
